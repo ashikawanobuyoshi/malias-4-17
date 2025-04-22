cc72acc91d00087dc2f319b1294e1e6eda7fa68b
芦川展好 <studiomalia1@gmail.com>
2025-04-22 23:02:06 +0900

    Delete lib directory

--- lib/useRequireAuth.ts
+++
@@ -1,7 +0,0 @@
-// lib/useRequireAuth.ts
-const useRequireAuth = () => {
-  // 仮の処理：常に認証済みとする
-  return { user: { email: 'test@example.com' }, isLoading: false };
-};
-
-export default useRequireAuth;
